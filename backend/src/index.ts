df20852dc00676fd194b7c0088c271b594820c48
Alexandros-o-megas <quivealexandre@gmail.com>
2026-09-04 15:39:00 +0200

    Add dotenv and y dependencies to backend and frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import cors from "cors";
 import express from "express";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 import {
   addSubmission,
   archiveRecord,
